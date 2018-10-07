760e4bd69f6e01ba0e5f2fb2e8e2b559eae81e05
Anwesh43 <anweshthecool0@gmail.com>
2018-10-07 20:27:29 +0530

    adding State class

---
+++ CircleMiddleLineGif.js
@@ -0,0 +1,28 @@
+const w = 500, h = 500
+const Canvas = require('canvas')
+const GifEncoder = require('gifencoder')
+const k = 3
+class State {
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += (0.1 / k) * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
